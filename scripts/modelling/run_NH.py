11ac9ee3ef3776857c27a82a2ed17b59110c91a5
wkeenan21 <wkeenan21@gmail.com>
2025-11-06 13:18:18 -0700

    config

--- scripts/modelling/run_NH.py
+++ scripts/modelling/run_NH.py
@@ -6,4 +6,4 @@ import torch
 from neuralhydrology.evaluation import metrics
 from neuralhydrology.nh_run import start_run, eval_run
 
-start_run(config_file=Path(r"C:\Users\C830645719\OneDrive - Colostate\documents\GitHub\CO_natural_streamflow\scripts\configs\config1.yml"), gpu=-1)
+start_run(config_file=Path(r"scripts\configs\config1.yml"))
